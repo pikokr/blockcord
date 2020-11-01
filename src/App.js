00f24bd33bc8570fd6e25e372b4b84abfba379de
pikokr <parangee9706@gmail.com>
2020-11-01 21:42:31 +0900

    Feat: Initialize blockly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import blocks from "./blocks";
+import Blockly from "blockly";
 
 class App extends React.Component {
     componentDidMount() {
+        const workspace = Blockly.inject('editor', {
+            toolbox: document.getElementById('toolbox')
+        })
+        workspace.addChangeListener(e => console.log(Blockly.JavaScript.workspaceToCode(workspace)))
     }
 
     render() {
         return (
             <div>
+                <div id="editor" style={{
+                    position: 'absolute',
+                    width: '100vw',
+                    height: '100vh'
+                }}/>
+                <div dangerouslySetInnerHTML={{__html: blocks}}/>
             </div>
         )
     }
